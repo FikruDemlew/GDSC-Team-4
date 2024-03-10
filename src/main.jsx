c70a9ab930b27144d6184ec40c7d479cefbca536
FikruDemlew <fikrudemlew@gmail.com>
2024-03-11 00:03:32 +0300

    feat: added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Home from './page/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home />
+  },
+  {
+    path:"/app",
+    element:<App />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
