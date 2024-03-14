7f47b0214e2c378cb6b05b06f8b53f30251f1e08
FikruDemlew <fikrudemlew@gmail.com>
2024-03-14 21:31:57 +0300

    Create a dynamic Login and Sign-up page and added a firebase authentication

---
+++ src/firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOdzTTVgSmXqqHkgJog9rGt9Dx-xpQlGs",
+  authDomain: "auth-development-7a7f7.firebaseapp.com",
+  projectId: "auth-development-7a7f7",
+  storageBucket: "auth-development-7a7f7.appspot.com",
+  messagingSenderId: "577509244501",
+  appId: "1:577509244501:web:cecf779eba057efba5dedc",
+  measurementId: "G-HDWL3ME1JJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const analytics = getAnalytics(app);
+
+export {app,auth};
